cf8c19825c8de3380b8614bb9e6ed15e73e64486
jc <jc@hk>
2026-04-04 20:45:57 +0800

    update RULER in lm-eval

--- experiment/lm_eval/niah.sh
+++ experiment/lm_eval/niah.sh
@@ -5,7 +5,8 @@ MODEL_PATH="./qwen_mirror"
 TASK_NAME="niah_single_1_local"
 INCLUDE_PATH="./my_vllm/experiment/lm_eval/tasks"
 MAX_LENGTH=32768
-NUM_SAMPLE=50
+NUM_SAMPLE=200
+METADATA="{\"max_seq_lengths\":[4096,8192,16384,32768],\"num_samples_per_length\":${NUM_SAMPLE}}"
 
 
 echo "MODEL_PATH=${MODEL_PATH}"
@@ -16,7 +17,7 @@ echo "NUM_SAMPLE=${NUM_SAMPLE}"
 
 lm-eval \
   --model vllm \
-  --model_args "pretrained=${MODEL_PATH},max_length=${MAX_LENGTH}" \
+  --model_args "pretrained=${MODEL_PATH},max_length=${MAX_LENGTH},data_parallel_size=2" \
   --tasks "${TASK_NAME}" \
   --include_path "${INCLUDE_PATH}" \
-  --metadata '{"max_seq_lengths":[4096,8192,16384,32768],"num_samples_per_length":50}'
+  --metadata "${METADATA}"
